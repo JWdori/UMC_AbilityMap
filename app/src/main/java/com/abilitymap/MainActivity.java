b5dc6e4a395000c4096f03559272543eeddc194e
Dori <jaewan0114@naver.com>
2022-07-09 03:29:36 +0900

    전화 권한 불가능

--- app/src/main/java/com/abilitymap/MainActivity.java
+++ app/src/main/java/com/abilitymap/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
-    
+    static final int SMS_RECEIVE_PERMISSON=1;
     private LatLng currentPosition;
     List<DTO> items;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton Report_button = (ImageButton) findViewById(R.id.repot_message);
+        ImageButton Report_message = (ImageButton) findViewById(R.id.repot_message);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -186,8 +186,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 else
                     Toast.makeText(getBaseContext(), "전화번호와 메시지를 입력해주세요.", Toast.LENGTH_SHORT).show();
             }});
+
+
 */
+
+
     }
+
+
+
+
+
 
 
     private void sendSMS(String phoneNumber, String message)
@@ -736,6 +745,50 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     private void initClickListener() {
+
+
+        //긴급신고 메세지지
+       ImageButton Report_message = findViewById(R.id.repot_message);
+        Report_message.setOnClickListener(new View.OnClickListener() {
+            int permissonCheck= ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+            if(permissonCheck == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
+
+                //권한설정 dialog에서 거부를 누르면
+                //ActivityCompat.shouldShowRequestPermissionRationale 메소드의 반환값이 true가 된다.
+                //단, 사용자가 "Don't ask again"을 체크한 경우
+                //거부하더라도 false를 반환하여, 직접 사용자가 권한을 부여하지 않는 이상, 권한을 요청할 수 없게 된다.
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
+                    //이곳에 권한이 왜 필요한지 설명하는 Toast나 dialog를 띄워준 후, 다시 권한을 요청한다.
+                    Toast.makeText(getApplicationContext(), "SMS권한이 필요합니다", Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS},       SMS_RECEIVE_PERMISSON);
+                }else{
+                    ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS}, SMS_RECEIVE_PERMISSON);
+                }
+            }
+            @Override
+            public void onClick(View v) {
+                //입력한 값을 가져와 변수에 담는다
+                System.out.println("123456");
+                String phoneNo = "123";
+                String sms = "123";
+
+
+                try {
+                    //전송
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "전송 완료!", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "SMS faild, please try again later!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
 
         binding.layoutToolBar.ivMenu.setOnClickListener(new View.OnClickListener(){
             @Override
