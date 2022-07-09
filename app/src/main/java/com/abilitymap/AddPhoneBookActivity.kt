66f38ca99482a494d63ddfd7bbae5af1f0e3cd2b
hkjeong2 <chali0330@gmail.com>
2022-07-09 17:54:17 +0900

    긴급 연락처 화면 짤림 수정, 연락처 추가 화면 수정

--- app/src/main/java/com/abilitymap/AddPhoneBookActivity.kt
+++ app/src/main/java/com/abilitymap/AddPhoneBookActivity.kt
@@ -1,6 +1,9 @@
 package com.abilitymap
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -26,16 +29,37 @@ class AddPhoneBookActivity : AppCompatActivity() {
         binding.tvSaveButtonAddPhoneBook.setOnClickListener {
             addValidInfoToDB()
         }
+        binding.etNameAddPhoneBook.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.tvNameNumberAddPhoneBook.text = "0/8"
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.tvNameNumberAddPhoneBook.text = binding.etNameAddPhoneBook.text.toString().length.toString() + "/8 자"
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.tvNameNumberAddPhoneBook.text = binding.etNameAddPhoneBook.text.toString().length.toString() + "/8 자"
+            }
+
+        })
     }
 
     private fun addValidInfoToDB(){
         if (binding.etNameAddPhoneBook.text.toString().isEmpty() || binding.etPhoneNumberAddPhoneBook.text.toString().isEmpty()){
-            if (binding.etNameAddPhoneBook.text.toString().isEmpty() && binding.etPhoneNumberAddPhoneBook.text.toString().isEmpty())
+            if (binding.etNameAddPhoneBook.text.toString().isEmpty() && binding.etPhoneNumberAddPhoneBook.text.toString().isEmpty()){
                 Toast.makeText(this,"저장할 정보를 모두 입력해 주세요", Toast.LENGTH_SHORT).show()
-            else if (binding.etPhoneNumberAddPhoneBook.text.toString().isEmpty())
+                binding.tvNameMustHaveAddPhoneBook.setTextColor(Color.parseColor("#FF0000"))
+                binding.tvPhoneNumberMustHaveAddPhoneBook.setTextColor(Color.parseColor("#FF0000"))
+            }
+            else if (binding.etPhoneNumberAddPhoneBook.text.toString().isEmpty()){
                 Toast.makeText(this,"저장할 번호를 입력해 주세요", Toast.LENGTH_SHORT).show()
-            else
+                binding.tvPhoneNumberMustHaveAddPhoneBook.setTextColor(Color.parseColor("#FF0000"))
+            }
+            else{
                 Toast.makeText(this,"저장할 이름을 입력해 주세요", Toast.LENGTH_SHORT).show()
+                binding.tvNameMustHaveAddPhoneBook.setTextColor(Color.parseColor("#FF0000"))
+            }
         }
         else{       //정보가 모두 기입 됐을 시
             personInfoDatabase.personInfoDao().insertPerson(
