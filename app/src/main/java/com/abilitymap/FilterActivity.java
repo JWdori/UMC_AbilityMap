fdc6b670d7717d2c6edcadeccd548c959089ebfe
Dori <jaewan0114@naver.com>
2022-07-15 04:08:28 +0900

    상태바 수정. 필터 추가 중

---
+++ app/src/main/java/com/abilitymap/FilterActivity.java
@@ -0,0 +1,18 @@
+package com.abilitymap;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
