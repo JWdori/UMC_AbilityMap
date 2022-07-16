140140a0c0942c7e657e0dd915cc95701de131e1
JungAh <70677614+Jungahgo@users.noreply.github.com>
2022-07-16 19:57:36 +0900

    제보 조회 API
    제보 조회 API

--- server/backend/src/app/Report/reportRoute.js
+++ server/backend/src/app/Report/reportRoute.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
     //4.0 제보 생성 API 
     console.log("Route.js");
     app.post('/report', report.postReport);
+    
+     //4.1 제보 조회 API
+    app.get('/report', report.getReport);
+
 };
