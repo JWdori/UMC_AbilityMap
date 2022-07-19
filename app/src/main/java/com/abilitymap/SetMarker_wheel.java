e068769a26c9d2c692a2ba9c0c75160d19d1277c
Dori <jaewan0114@naver.com>
2022-07-19 09:52:45 +0900

    승강기 추가

--- app/src/main/java/com/abilitymap/SetMarker_wheel.java
+++ app/src/main/java/com/abilitymap/SetMarker_wheel.java
@@ -1,5 +1,8 @@
 package com.abilitymap;
 
+import android.content.Context;
+import android.os.Handler;
+
 import androidx.annotation.NonNull;
 
 import com.naver.maps.geometry.LatLng;
@@ -13,17 +16,52 @@ import com.naver.maps.map.overlay.OverlayImage;
 
 public interface SetMarker_wheel extends Overlay.OnClickListener {
 
-    public default void setMarker_wheel(double x, double y, String markerType, NaverMap naverMap){
+    public default void setMarker_wheel(double x, double y, String markerType, NaverMap naverMap, String z){
         Marker marker = new Marker();
         marker.setPosition(new LatLng(x,y));
         marker.setWidth(80);
         marker.setHeight(80);
         marker.setMinZoom(9);//줌 설정
         switch(markerType){
-            case "wheel": marker.setIcon(OverlayImage.fromResource(R.drawable.wheel_icon)); break;
+            case "ele": marker.setIcon(OverlayImage.fromResource(R.drawable.wheel_icon)); break;
             case "slope": marker.setIcon(OverlayImage.fromResource(R.drawable.danger_location_yellow)); break;
         }
         marker.setMap(naverMap);
+        InfoWindow infoWindow = new InfoWindow();
+        marker.setTag(z);
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter((Context) this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                // 정보 창이 열린 마커의 tag를 텍스트로 노출하도록 반환
+                return (CharSequence)infoWindow.getMarker().getTag();
+            }
+        });
+        infoWindow.setAlpha(0.8f);
+        Overlay.OnClickListener listener = overlay -> {
+            naverMap.setOnMapClickListener((coord, point) -> {
+                infoWindow.close();
+            });
+            if (marker.getInfoWindow() == null) {
+                // 현재 마커에 정보 창이 열려있지 않을 경우 엶
+                infoWindow.open(marker);
+                Handler handler = new Handler();
+                if(marker.getInfoWindow() != null){
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            infoWindow.close();
+                        }
+                    },3000);	//3초 동안 딜레이
+                }
+            } else {
+                // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
+                infoWindow.close();
+            }
+
+            return true;
+        };
+        marker.setOnClickListener(listener);
 
         marker.setOnClickListener(this);
     }
