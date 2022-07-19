a46aa9b1c86e1abe7f204e4521cce6b8f7abb5d8
hkjeong2 <chali0330@gmail.com>
2022-07-19 14:44:16 +0900

    text 부분 bold

--- app/src/main/java/com/abilitymap/ChooseOptionActivity.kt
+++ app/src/main/java/com/abilitymap/ChooseOptionActivity.kt
@@ -1,6 +1,11 @@
 package com.abilitymap
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.StyleSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.abilitymap.databinding.ActivityChooseOptionBinding
 
@@ -13,7 +18,27 @@ class ChooseOptionActivity :AppCompatActivity() {
         binding = ActivityChooseOptionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        changeText()
+    }
 
+
+    private fun changeText(){   //text 부분 bold 처리
+        val title : String = binding.tvTitleChooseOption.text.toString()
+        val yesOption : String = binding.tvYesChooseOption.text.toString()
+        val noOption : String = binding.tvNoChooseOption.text.toString()
+
+        var builder = SpannableStringBuilder(title)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        builder.setSpan(boldSpan, 4, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvTitleChooseOption.text = builder
+
+        builder = SpannableStringBuilder(yesOption)
+        builder.setSpan(boldSpan, 3, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvYesChooseOption.text = builder
+
+        builder = SpannableStringBuilder(noOption)
+        builder.setSpan(boldSpan, 9, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvNoChooseOption.text = builder
     }
 
 }
