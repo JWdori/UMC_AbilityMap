1dadf2ca2f87b024ae08635f4c45ed246ad52efa
Dori <jaewan0114@naver.com>
2022-07-20 03:45:06 +0900

    api추가, 필터 디자인

--- app/src/main/java/com/abilitymap/SplashActivity.java
+++ app/src/main/java/com/abilitymap/SplashActivity.java
@@ -21,27 +21,27 @@ public class SplashActivity extends AppCompatActivity {
 
                 startActivity(new Intent(getApplicationContext(), OnboardingActivity.class));
 
-//                if (isFirst){   //최초 실행일 시 onBoarding 화면 pop up
-//
-//                    //onBoarding 화면으로 이동
-//                    startActivity(new Intent(getApplicationContext(), OnboardingActivity.class));
-//
-//                    finish();
-//
-//                    spfOnBoarding = getSharedPreferences("onBoarding", MODE_PRIVATE);
-//                    SharedPreferences.Editor editor = spfOnBoarding.edit();
-//                    editor.putBoolean("isFirst", false);
-//                    editor.apply();
-//                    editor.commit();    //이후 실행부터는 else문으로 가도록 isFirst == false로 지정
-//                }
-//                else{
-//                    //new Intent(현재 context, 이동할 activity)
-//                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//
-//                    startActivity(intent);    //intent 에 명시된 액티비티로 이동
-//
-//                    finish();    //현재 액티비티 종료
-//                }
+                if (isFirst){   //최초 실행일 시 onBoarding 화면 pop up
+
+                    //onBoarding 화면으로 이동
+                    startActivity(new Intent(getApplicationContext(), OnboardingActivity.class));
+
+                    finish();
+
+                    spfOnBoarding = getSharedPreferences("onBoarding", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = spfOnBoarding.edit();
+                    editor.putBoolean("isFirst", false);
+                    editor.apply();
+                    editor.commit();    //이후 실행부터는 else문으로 가도록 isFirst == false로 지정
+                }
+                else{
+                    //new Intent(현재 context, 이동할 activity)
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                    startActivity(intent);    //intent 에 명시된 액티비티로 이동
+
+                    finish();    //현재 액티비티 종료
+                }
             }
         }, 1000);
     }
