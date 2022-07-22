cd1a70d08ab521b6da71f588aa0e5b371a57e578
hkjeong2 <chali0330@gmail.com>
2022-07-22 15:10:42 +0900

    이용 설명서 3, 4 페이지 완

--- app/src/main/java/com/abilitymap/MenuBookFourthFragment.kt
+++ app/src/main/java/com/abilitymap/MenuBookFourthFragment.kt
@@ -1,6 +1,13 @@
 package com.abilitymap
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,8 +25,24 @@ class MenuBookFourthFragment : Fragment() {
     ): View? {
         binding = FragmentMenuBookFourthBinding.inflate(inflater, container, false)
 
+        init()
+
         return binding.root
     }
+
+    private fun init(){
+        val title : String = binding.tvFragmentMenuBookFourth.text.toString()
+
+        var builder = SpannableStringBuilder(title)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val sizeSpan = RelativeSizeSpan(0.8f)
+        val colorSpan = ForegroundColorSpan(Color.parseColor("#707070"))
+        builder.setSpan(boldSpan, binding.tvFragmentMenuBookFourth.text.indexOf("위험"), binding.tvFragmentMenuBookFourth.text.indexOf("이동권과")+7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(sizeSpan, binding.tvFragmentMenuBookFourth.text.indexOf("사진"), binding.tvFragmentMenuBookFourth.text.toString().length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(colorSpan, binding.tvFragmentMenuBookFourth.text.lastIndexOf("사진"), binding.tvFragmentMenuBookFourth.text.toString().length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvFragmentMenuBookFourth.text = builder
+    }
+
 
 
 }
--- app/src/main/java/com/abilitymap/MenuBookThirdFragment.kt
+++ app/src/main/java/com/abilitymap/MenuBookThirdFragment.kt
@@ -1,6 +1,13 @@
 package com.abilitymap
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +25,22 @@ class MenuBookThirdFragment : Fragment() {
     ): View? {
         binding = FragmentMenuBookThirdBinding.inflate(inflater, container, false)
 
+        init()
+
         return binding.root
+    }
+
+    private fun init(){
+        val title : String = binding.tvFragmentMenuBookThird.text.toString()
+
+        var builder = SpannableStringBuilder(title)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val sizeSpan = RelativeSizeSpan(0.8f)
+        val colorSpan = ForegroundColorSpan(Color.parseColor("#707070"))
+        builder.setSpan(boldSpan, binding.tvFragmentMenuBookThird.text.indexOf("긴급"), binding.tvFragmentMenuBookThird.text.indexOf("긴급")+6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(sizeSpan, binding.tvFragmentMenuBookThird.text.lastIndexOf("긴급"), binding.tvFragmentMenuBookThird.text.toString().length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(colorSpan, binding.tvFragmentMenuBookThird.text.lastIndexOf("긴급"), binding.tvFragmentMenuBookThird.text.toString().length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvFragmentMenuBookThird.text = builder
     }
 
 
