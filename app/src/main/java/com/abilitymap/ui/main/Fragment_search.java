de23ed7e2f17ed932c25494de96e1f3604177ffe
Dori <jaewan0114@naver.com>
2022-08-15 10:51:24 +0900

    검색 인터페이스

--- app/src/main/java/com/abilitymap/ui/main/Fragment_search.java
+++ app/src/main/java/com/abilitymap/ui/main/Fragment_search.java
@@ -3,19 +3,30 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.abilitymap.R;
 
-public class Fragment_search extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fragment_search extends Fragment implements Search_ItemAdapter.onItemListener {
         ImageView searchback;
+        private Search_ItemAdapter adapter;
+        private List<Search_Item> itemList;
+
 
         public Fragment_search() {
                 // Required empty public constructor
@@ -25,15 +36,30 @@ public class Fragment_search extends Fragment {
         public void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
 
-
         }
 
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-
-
                 View view = inflater.inflate(R.layout.search, container, false);
+                RecyclerView recyclerView = view.findViewById(R.id.search_result);
+                recyclerView.setHasFixedSize(true);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+                //adapter
+                itemList = new ArrayList<>(); //샘플테이터
+                fillData();
+                adapter = new Search_ItemAdapter(itemList);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL); //밑줄
+                recyclerView.addItemDecoration(dividerItemDecoration);
+
+                //데이터셋변경시
+                //adapter.dataSetChanged(exampleList);
+
+                //어댑터의 리스너 호출
+                adapter.setOnClickListener(this);
+
                 searchback = view.findViewById(R.id.search_back);
                 searchback.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -48,4 +74,47 @@ public class Fragment_search extends Fragment {
 
 
 
+
+        private void fillData() {
+                itemList = new ArrayList<>(); //샘플테이터
+                itemList.add(new Search_Item(R.drawable.hos_icon, "One", "Ten"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Two", "Eleven"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Three", "Twelve"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Four", "Thirteen"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Five", "Fourteen"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Six", "Fifteen"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Seven", "Sixteen"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Eight", "Seventeen"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Nine", "Eighteen"));
+                itemList.add(new Search_Item(R.drawable.hos_icon, "Nine", "Eighteen"));
+        }
+//        @Override
+//        public boolean onCreateOptionsMenu(Menu menu) {
+//
+//                getMenuInflater().inflate(R.menu.menu, menu);
+//                MenuItem searchItem = menu.findItem(R.id.action_search);
+//                SearchView searchView = (SearchView) searchItem.getActionView();
+//
+//                searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+//
+//                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//                        @Override
+//                        public boolean onQueryTextSubmit(String query) {
+//                                return false;
+//                        }
+//
+//                        @Override
+//                        public boolean onQueryTextChange(String newText) {
+//                                adapter.getFilter().filter(newText);
+//                                return false;
+//                        }
+//                });
+//                return super.onCreateOptionsMenu(menu);
+//        }
+
+
+        @Override
+        public void onItemClicked(int position) {
+                Toast.makeText(getContext(), "" + position, Toast.LENGTH_SHORT).show();
+        }
 }
---
+++ app/src/main/java/com/abilitymap/ui/main/Search_ItemAdapter.java
@@ -0,0 +1,162 @@
+package com.abilitymap.ui.main;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.abilitymap.R;
+import com.abilitymap.ui.main.Search_Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***********************************************************************************************
+ mtp1
+ ItemAdapter		=> ToDoAdapter			어댑터이름
+ ItemViewHolder		=> ToDoViewHolder		뷰홀더이름
+ onItemListener	=> onToDoClickListener 	인터페이스이름
+ onItemClicked 		=> onToDoClicked 		인터페이스 클릭메서드이름
+ row_item		=> list_item 			아이템레이아웃이름
+ ItemModel		=> Person 				모델이름
+
+ **********************************************************************************************/
+
+public class Search_ItemAdapter extends RecyclerView.Adapter<Search_ItemAdapter.ItemViewHolder> implements Filterable {
+
+    private List<Search_Item> mDataList;
+    private List<Search_Item> mDataListAll;
+
+    //constructor
+    public Search_ItemAdapter(List<Search_Item> items) {
+        mDataList = items;
+        mDataListAll = new ArrayList<>(items);
+    }
+
+    //interface - 클릭인터페이스
+    private onItemListener mListener;
+    public void setOnClickListener(onItemListener listener){
+        mListener = listener;
+    }
+
+    //data set changed
+    public void dataSetChanged(List<Search_Item> exampleList) {
+        mDataList = exampleList;
+        notifyDataSetChanged();
+    }
+
+    //1.onCreateViewHolder -------------------------------------------------------
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_result,
+                parent, false);
+        return new ItemViewHolder(v);
+    }
+
+    //2.onBindViewHolder  -------------------------------------------------------
+    @Override
+    public void onBindViewHolder(@NonNull final ItemViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        Search_Item currentItem = mDataList.get(position);
+        // TODO : 데이터를 뷰홀더에 표시하시오
+        holder.imageView.setImageResource(currentItem.getImageResource());
+        holder.textView1.setText(currentItem.getText1());
+        holder.textView2.setText(currentItem.getText2());
+        holder.textView3.setText(currentItem.getText3());
+
+        // TODO : 리스너를 정의하시오.
+        if (mListener != null){
+            final int pos = position;
+            //final ItemModel item = mItems.get(viewHolder.getAdapterPosition());
+            holder.itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    mListener.onItemClicked(position);
+                    //mListener.onItemClicked(item);
+                }
+            });
+            //버튼등에도 동일하게 지정할 수 있음 holder.버튼이름.setOnClickListener..형식으로
+        }
+    }
+
+    //3.getItemCount  -------------------------------------------------------
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+
+    // 데이터 필터 검색 Filterable implement ---------------------------------
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+        //Automatic on background thread
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Search_Item> filteredList = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(mDataListAll);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (Search_Item item : mDataListAll) {
+                    //TODO filter 대상 setting
+                    if (item.getText2().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        //Automatic on UI thread
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mDataList.clear();
+            mDataList.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    // 뷰홀더 클래스  ---------------------------------
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+        // TODO : 뷰홀더 완성하시오
+        ImageView imageView;
+        TextView textView1;
+        TextView textView2;
+        TextView textView3;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+            // TODO : 뷰홀더 완성하시오
+            imageView = itemView.findViewById(R.id.search_marker);
+            textView1 = itemView.findViewById(R.id.search_title);
+            textView2 = itemView.findViewById(R.id.search_location);
+            textView3 = itemView.findViewById(R.id.search_tag);
+        }
+    }
+
+    //onclick listener interface
+    //1. interface onItemListener 선언
+    //2. 내부에서 mListener 선언하고
+    // 외부에서 접근가능하도록 setOnClickListener작성
+    //3.onBindViewHolder에서 처리
+    public interface onItemListener {
+        void onItemClicked(int position);
+        //void onItemClicked(ItemModel model); 모델값을 넘길수 있음
+        //다른버튼도 정의할 수 있음 onShareButtonClicked(int position);
+    }
+}
